89f4bed5fc4e9d1e838d1950744c849b1b35bb94
juarezlucasa <juarezlucas.92@gmail.com>
2021-09-20 19:58:33 -0300

    Cambios output

--- create-ec2-instance/output.tf
+++ create-ec2-instance/output.tf
@@ -2,6 +2,6 @@ output "aws_security_group_http_server_details" {
   value = aws_security_group.poc_patch_manager_sg
 }
 
-output "http_server_public_dns" {
+output "poc-patch-manager_public_dns" {
   value = aws_instance.poc-patch-manager.public_dns
 }
